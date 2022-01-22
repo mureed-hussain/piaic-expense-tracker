dff570ad99f26b043084b6cac93d4d31e5e2ad98
Mureed Hussain <imhussain0086@gmail.com>
2022-01-22 23:08:02 +0500

    UI completed
    UI completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncExp } from "./components/IncExp";
+import { Transcition } from "./components/Transcition";
+import { AddTrans } from "./components/AddTrans";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncExp />
+        <Transcition />
+        <AddTrans />
+      </div>
     </div>
   );
 }
