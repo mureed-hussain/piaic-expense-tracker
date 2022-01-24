4170fd3a891b0e9ab7ff90935495437c286bc7df
Mureed Hussain <imhussain0086@gmail.com>
2022-01-24 20:03:54 +0500

    src updated
    src updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import "./App.css";
-import { Header } from "./components/Header";
-import { Balance } from "./components/Balance";
-import { IncExp } from "./components/IncExp";
-import { Transaction } from "./components/Transcation";
-import { AddTrans } from "./components/AddTrans";
-
-import { GlobalProvider } from "./context/GlobalState";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <GlobalProvider>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncExp />
-        <Transaction />
-        <AddTrans />
-      </div>
-    </GlobalProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
